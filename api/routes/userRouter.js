b1e2957f957e3a72b422287f39798a6f3d01f18e
Pourixa <pouria.footballer@gmail.com>
2026-08-23 13:55:46 +0200

    Init user router

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,9 +1,13 @@
 const userRouter = require("express").Router()
 
-userRouter.post("/signin",signinUser) // sign in  
-userRouter.post("/signup",signupUser) // signup user
+userRouter.patch("/",updateUser)  //authenticate update user
+userRouter.delete("/",deleteUser)  //authenticate delete user
+
 userRouter.get("/:username",getUser)  // get user by username
-userRouter.patch("/:username",updateUser)  // get user by username
+
+userRouter.post("/auth/signin",signinUser) // sign in  
+userRouter.post("/auth/signup",signupUser) // signup user
+userRouter.post("/search/username",searchUser) // search user by query username
 
 
 
