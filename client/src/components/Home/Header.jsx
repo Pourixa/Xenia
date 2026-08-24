5ccc80c76c1d6eb41af4b26fecc5e2cad4a6bddc
Pourixa <pouria.footballer@gmail.com>
2026-08-24 15:47:28 +0200

    Add random avatars

--- client/src/components/Home/Header.jsx
+++ client/src/components/Home/Header.jsx
@@ -4,7 +4,7 @@ import { XeniaLogoNoName } from "../customUI/XeniaLogoNoName";
 
 export function Header() {
     return <header className="border-b-2 flex justify-between items-center p-1 pl-5 pr-5">
-        <XeniaAvatar size="sm"/>
+        <XeniaAvatar size="lg"/>
         <XeniaLogoNoName width={56} height={56}/>
         <Bell width={24} height={24}/>
     </header>
--- client/src/components/customUI/XeniaAvatar.jsx
+++ client/src/components/customUI/XeniaAvatar.jsx
@@ -1,14 +1,26 @@
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useMemo } from "react";
+import { Style, Avatar as DiceBearAvatar } from "@dicebear/core";
+import lineFace from "@dicebear/styles/line-face.json" with { type: "json" };
+const style = new Style(lineFace);
 
-export function XeniaAvatar({size="default",imageSrc=null,alt="",fallback="A"}) {
-    return <Avatar size={size}>
-        <AvatarImage
-        src={imageSrc}
-        alt={alt}
-        >
-        <AvatarFallback>
-            {fallback}
-        </AvatarFallback>
-        </AvatarImage>
+export function XeniaAvatar({
+  size = "default",
+  imageSrc = null,
+  alt = "",
+  name = "",
+}) {
+  const generatedAvatar = useMemo(() => {
+    return new DiceBearAvatar(style, {
+      seed:name,
+      size: 128,
+    }).toDataUri();
+  }, [name]);
+  const avatar = imageSrc ?? generatedAvatar;
+  return (
+    <Avatar size={size}>
+      <AvatarImage src={avatar} alt={alt}></AvatarImage>
+      <AvatarFallback>{name.slice(0, 1)}</AvatarFallback>
     </Avatar>
+  );
 }
