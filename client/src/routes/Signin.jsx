8e820b3d85ae778234378f428ff19643d614d3a3
Pourixa <pouria.footballer@gmail.com>
2026-08-27 10:10:53 +0200

    Create signup and signin pages

--- client/src/routes/Signin.jsx
+++ client/src/routes/Signin.jsx
@@ -1,3 +1,5 @@
+import { LoginForm } from "@/components/login-form";
+
 export function Signin() {
-    
+    return <LoginForm className={"h-dvh justify-center"}/>
 }
--- client/src/routes/Signup.jsx
+++ client/src/routes/Signup.jsx
@@ -1,3 +1,5 @@
+import { SignupForm } from "@/components/signup-form";
+
 export function Signup() {
-    
+    return <SignupForm className={"justify-center h-dvh"}/>
 }
