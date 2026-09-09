9d68d80e2e2d3fcb585abc7cbcac27afde49ab7e
Pourixa <72301308+Pourixa@users.noreply.github.com>
2026-09-09 13:29:42 +0000

    Set up on code base

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const { errorHandler } = require("./middleware/utils")
 const userRouter = require("./routes/userRouter")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
-
+require("dotenv").config()
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
